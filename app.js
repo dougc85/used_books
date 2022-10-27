5f71a3fb85f373ea04f29ef397008d95407806d7
Doug Carter <dougc85@gmail.com>
2022-10-26 21:45:57 -0700

    1st commit; app skeleton

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const secrets = require('./secrets');
+
+const get404 = require('./controllers/404Controller');
+const getIndex = require('./controllers/indexController');
+
+const app = express();
+const mongoDB = secrets.mongoURI;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get("/", getIndex);
+app.use("/", get404);
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log('Cannot connect to DB');
+  })
+
