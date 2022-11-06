30135db3a5a86b4dc6aaad9ef45af071a5486aa5
Doug Carter <dougc85@gmail.com>
2022-11-05 20:08:32 -0700

    finished generating checkboxes in admin edit picks

--- controllers/adminControllers/frontPageController.js
+++ controllers/adminControllers/frontPageController.js
@@ -1,9 +1,18 @@
+const Book = require('../../models/book');
+
 exports.getFrontPage = (req, res, next) => {
   res.render('admin/frontPageAdmin', { backText: 'Admin', backHref: '/admin' });
 }
 
 exports.getEditPicksFront = (req, res, next) => {
-
+  Book
+    .find()
+    .select('title imageURL')
+    .sort({ "title": 1 })
+    .then((books) => {
+      res.render('admin/editPicks', { books, backText: 'Front Page Admin', backHref: '/admin/frontpage' });
+    })
+    ;
 }
 
 exports.postEditPicksFront = (req, res, next) => {
